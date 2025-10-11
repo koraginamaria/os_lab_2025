779aa4711ccc1db9b7622e56cc3e0d4312a68350
koraginamaria <koraginamaria93@gmail.com>
2025-10-11 09:26:06 +0000

    lab2 ok

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,18 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str);
+    int i = 0;
+    int j = length - 1;
+    
+    while (i < j) {
+        
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        
+        i++;
+        j--;
+    }
 }
 
